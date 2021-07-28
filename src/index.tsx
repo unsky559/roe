3595a30db08133a2d5697a50e1109502add35c99
unsky559 <6kostya@gmail.com>
2021-07-28 09:40:16 +0300

    feat: add router component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,37 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import { PageLanding } from './page/page-landing/page.landing';
+
+/**
+ * Welcome to ROE! 
+ * This is an entry point of a project
+ * 
+ * All routes goes through Router component, that display needed page
+ */
 
 ReactDOM.render(
-  <h1>Привет, мир!</h1>,
-  document.getElementById('root')
-  
+  <App></App>,
+  document.body
 );
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <PageLanding />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}

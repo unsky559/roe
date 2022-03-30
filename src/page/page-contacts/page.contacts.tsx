062c6fbcd73e9c19fa4e65e9340ea370522c760a
unsky559 <6kostya@gmail.com>
2022-03-30 15:38:27 +0300

    fead: contacts page

---
+++ src/page/page-contacts/page.contacts.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function PageContactas(){
+    return (
+        <div className="container">
+            <h1>Page COntacts</h1>
+        </div>
+    )
+}

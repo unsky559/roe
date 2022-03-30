ce77e1fd8cdafade57b4bc1c16c8a50c6f0e9202
unsky559 <6kostya@gmail.com>
2022-03-30 15:39:32 +0300

    fead: 404 page

---
+++ src/page/page-404/page.404.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Page404(){
+    return (
+        <div className="container">
+            <h1>404 page - error</h1>
+        </div>
+    )
+}

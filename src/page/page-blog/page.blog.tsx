60e871d976b9ffd10b20e048eed27d335e36db53
unsky559 <6kostya@gmail.com>
2022-03-31 15:01:28 +0300

    feat: page blog markup

--- src/page/page-blog/page.blog.tsx
+++ src/page/page-blog/page.blog.tsx
@@ -1,9 +1,27 @@
 import * as React from "react";
+import ArticleCard from "../../components/article-card/article-card";
+import Searchbar from "../../components/searchbar/searchbar";
+import "./page.blog.scss";
 
 export default function PageBlog(){
     return (
-        <div className="container">
-            <h1>PageBlog</h1>
+        <div className="page-blog">
+            <section className="search-area">
+                <div className="container">
+                    <Searchbar />
+                </div>
+            </section>
+            <section className="articles">
+                <div className="container">
+                    <h1>Articles</h1>
+                    <div className="articles">
+                        <ArticleCard />
+                        <ArticleCard />
+                        <ArticleCard />
+
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }

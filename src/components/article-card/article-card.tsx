42f241f9a2470e184dece8f7bfa47d64d7d3ccb7
unsky559 <6kostya@gmail.com>
2022-04-06 14:05:37 +0300

    feat: add props type

--- src/components/article-card/article-card.tsx
+++ src/components/article-card/article-card.tsx
@@ -1,19 +1,32 @@
 import * as React from "react";
 import "./article-card.scss";
 
-export default function ArticleCard(){
+type propsType = {
+    cardData: {
+        imgURL: string,
+        title: string,
+        author: string,
+        date: Date,
+        previewText: string
+    }
+}
+type stateType = {}
+
+export default function ArticleCard(props: propsType){
     return(
         <div className="article-card">
             <div className="article-card-image">
-                <img src="https://images.unsplash.com/photo-1591267990532-e5bdb1b0ceb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1127&q=80" alt="" />
+                <img src={props.cardData.imgURL} alt={props.cardData.title} />
             </div>
             <div className="article-card-content">
-                <h2 className="article-card-title">Test</h2>
+                <h2 className="article-card-title">{props.cardData.title}</h2>
+                <p className="article-card-description">
+                    {props.cardData.previewText}
+                </p>
                 <div className="article-card-autor">
-                    <a href="">@unsky559</a>
-                    <time>28.10.2021</time>
+                    <a href="">@{props.cardData.author}</a>
+                    <time>{props.cardData.date.toLocaleDateString()}</time>
                 </div>
-                <p className="article-card-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, nemo suscipit atque molestias est id totam distinctio enim sed accusamus? Lorem elit. Nisi quis natus blanditiis ut vel similique.</p>
             </div>
         </div>
     );
--- src/page/page-blog/page.blog.tsx
+++ src/page/page-blog/page.blog.tsx
@@ -3,6 +3,40 @@ import ArticleCard from "../../components/article-card/article-card";
 import Searchbar from "../../components/searchbar/searchbar";
 import "./page.blog.scss";
 
+
+const mockData = {
+    posts: [
+        {
+            imgURL: `https://picsum.photos/seed/${Math.random()}/550/350`,
+            title: "Article 1",
+            author: "unsky559",
+            date: new Date(),
+            previewText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa recusandae cum obcaecati impedit itaque minus eos non amet nostrum!"
+        },
+        {
+            imgURL: `https://picsum.photos/seed/${Math.random()}/550/350`,
+            title: "Article 2 with a big and aws title sadasd asd sad asd asd asd ",
+            author: "unsky559",
+            date: new Date(),
+            previewText: "Lorem elit. Ut ipsa recusandae cum obcaecati impedit itaque minus eos non amet nostrum!"
+        },
+        {
+            imgURL: `https://picsum.photos/seed/${Math.random()}/550/350`,
+            title: "Article 1",
+            author: "unsky559",
+            date: new Date(),
+            previewText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ipsa recusandae cum obcaecati impedit itaque minus eos non amet nostrum!"
+        },
+        {
+            imgURL: `https://picsum.photos/seed/${Math.random()}/550/350`,
+            title: "Article 2",
+            author: "unsky559",
+            date: new Date(),
+            previewText: "Lorem elit. Ut ipsa recusandae cum obcaecati impedit itaque minus eos non amet nostrum!"
+        },
+    ]
+}
+
 export default function PageBlog(){
     return (
         <div className="page-blog">
@@ -15,10 +49,12 @@ export default function PageBlog(){
                 <div className="container">
                     <h1>Articles</h1>
                     <div className="articles">
-                        <ArticleCard />
-                        <ArticleCard />
-                        <ArticleCard />
-
+                        {
+                            mockData.posts.map((post) => {
+                                return (<ArticleCard cardData={post}/>)
+                            })
+                        }
+                        
                     </div>
                 </div>
             </section>

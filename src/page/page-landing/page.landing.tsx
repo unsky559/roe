e8a1f60221dfa4815c587059721fdc9d3e29dbf5
unsky559 <6kostya@gmail.com>
2022-04-06 12:55:10 +0300

    feat: landing page

--- src/page/page-landing/page.landing.tsx
+++ src/page/page-landing/page.landing.tsx
@@ -1,12 +1,19 @@
 import * as React from "react";
+import Button from "../../components/button/button";
 import "./page.landing.scss";
 
 export function PageLanding() {
     return (
-        <div>
-            <div className="container">
-                <h1>Page landing</h1>
-            </div>
+        <div className="page-landing">
+            <section className="banner-type">
+                <div className="container">
+                    <h1>My name is UNSKY559</h1>
+                    <div className="btn-links">
+                        <Button>Go to blog</Button>
+                        <Button>Contact me</Button>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }

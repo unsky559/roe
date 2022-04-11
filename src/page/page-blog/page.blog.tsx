5885b91e7fd9e50d818241e01051dee49cb3311e
unsky559 <6kostya@gmail.com>
2022-04-11 22:06:02 +0300

    feat: get posts from firebase

--- src/page/page-blog/page.blog.tsx
+++ src/page/page-blog/page.blog.tsx
@@ -4,6 +4,20 @@ import ArticleCard from "../../components/article-card/article-card";
 import Searchbar from "../../components/searchbar/searchbar";
 import "./page.blog.scss";
 
+import { initializeApp } from "firebase/app";
+import {collection, getDocs, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD_iq2Tl8MyUBnhoST2Bkbo9KEmjbJs2q0",
+    authDomain: "roe-blog.firebaseapp.com",
+    projectId: "roe-blog",
+    storageBucket: "roe-blog.appspot.com",
+    messagingSenderId: "775369680990",
+    appId: "1:775369680990:web:2e7e9a98d77d24ef21accf"
+};
+
+
+
 
 const mockData = {
     posts: [
@@ -48,7 +62,24 @@ type postType = {
 
 const getPosts = () : Promise<Array<postType>> => {
     return new Promise((resolve, reject)=>{
-        resolve(mockData.posts);
+
+        const app = initializeApp(firebaseConfig);
+
+        const db = getFirestore(app);
+
+        const querySnapshot = getDocs(collection(db, "posts")).then((data) => {
+            const accum: Array<postType> = [];
+            data.forEach((doc) => {
+                const data = doc.data();
+                data.date = data.date.toDate();
+
+                accum.push(data as postType);
+            });
+            console.log(accum);
+            resolve(accum);
+        });
+        console.log(mockData.posts);
+        // resolve(mockData.posts);
     })
 }
 
@@ -59,7 +90,7 @@ export default function PageBlog(){
         getPosts().then((posts: Array<postType>) => {
             postsState[1](posts);
         })
-    });
+    }, []);
 
     return (
         <div className="page-blog">

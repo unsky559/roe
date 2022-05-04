aa7560833b2eedc9fa2a86640ba83c5f35f2e256
unsky559 <6kostya@gmail.com>
2022-05-04 19:06:59 +0300

    feat: logo component

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import "./logo.scss";
+
+const Logo = () => {
+    return (
+        <div className="logo">
+            <div className="goog">
+                <div className="g">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="o">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="o">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="g">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Logo;

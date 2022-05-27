c6f6c4b93e238b5a708c316d91d4f8fd7e1d5110
unsky559 <6kostya@gmail.com>
2022-05-27 20:57:03 +0300

    feat: change markup

--- src/page/page-landing/page.landing.tsx
+++ src/page/page-landing/page.landing.tsx
@@ -1,19 +1,28 @@
 import * as React from "react";
-import Button from "../../components/button/button";
 import "./page.landing.scss";
+import ArticlePreview from "../../components/article-preview/article-preview";
 
 export function PageLanding() {
     return (
         <div className="page-landing">
             <section className="banner-type">
                 <div className="container">
-                    <h1>My name is UNSKY559</h1>
-                    <div className="btn-links">
-                        <Button>Go to blog</Button>
-                        <Button>Contact me</Button>
+                    <div className="col">
+                        <h1>Welcome to goog.men</h1>
+                        <p>I have a lot of interesting ideas. And I also have this cute domain name. So why not combine this things?</p>
+                    </div>
+                </div>
+            </section>
+            <section className="last-articles">
+                <div className="container">
+                    <h2>Last articles</h2>
+                    <div className="articles-list">
+                        <ArticlePreview/>
+                        <ArticlePreview/>
+                        <ArticlePreview/>
                     </div>
                 </div>
             </section>
         </div>
-    )
+)
 }

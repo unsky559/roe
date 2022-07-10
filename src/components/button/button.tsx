15a5edbe8e4920e6f8fb58db85ac122c7209630c
unsky559 <6kostya@gmail.com>
2022-07-10 12:41:48 +0300

    feat: add onClick prop to Button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,13 +3,14 @@ import "./button.scss";
 
 type propsType = {
     iconURL?: string,
-    children?: string
+    children?: string,
+    onClick?: (event: any) => any;
 }
-type stateType = {}
+//type stateType = {}
 
 export default function Button(props: propsType) {
     return (
-        <button className="btn">
+        <button className="btn" onClick={props.onClick}>
             {props.iconURL ? <img className="btn_icon" src={props.iconURL} alt="icon" /> : <></>}
             {props.children}
         </button>
